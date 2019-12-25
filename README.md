65273acf8cee164626310722adad9ea8810d5432
Julien Nicouleaud <juliennicouleaud@yahoo.fr>
2019-12-25 19:04:51 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# switchhost
-Switch IP adresses easily in hosts file
+# SwitchHost
+Switch IP adresses easily in hosts file.
+Useful when your VM changes IP address when you change location for instance.
+
+## Requirements
+None, it's just a plain shell script to execute.
+
+## Usage
+- Edit the list of "locations" and "IP addresses" in the `switchhost.sh` file
+- Make a copy of `hosts-template-example` and rename it `hosts-template`
+- List the hosts you want to be dynamic in the `hosts-template` file
+- Keep `hosts-template` and `switchhost.sh` in the same folder
+- Change the value of the variable `hostpath` in `switchhost.sh` if your hosts file is not located in `/etc`
+- Set permissions on the script : `chmod u+x switchhost.sh`
+- Execute the script and select the location :
+
+    ```bash
+    sudo ./switchhost
+    ```
+
+## TODO
+- Add checks if files exist, etc. (https://linuxize.com/post/bash-check-if-file-exists/)
---
+++ switchhost.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+################
+## Check if root
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
+################
+## Read location and set IP
+
+echo "Where are you?"
+echo "1. Home"
+echo "2. Work"
+echo "3. Bonnet"
+echo "4. Honor share"
+echo "5. Host only"
+echo "6. Temp"
+
+read reponse
+case $reponse in
+    [1] | [home]*) ip="192.168.1.62";;
+    [2] | [Work]*) ip="192.168.35.165";;
+    [3] | [bonnet]*) ip="192.168.1.17";;
+    [4] | [honor]*) ip="192.168.43.94";;
+    [5] | [host]*) ip="192.168.56.102";;
+    [6] | [temp]*) ip="192.168.56.102";;
+    *) echo "Erreur de saisie"
+        exit 1;;
+esac
+
+
+################
+## Backup hosts file et remplace par le template
+
+hostpath="/etc/"
+
+echo "Backup current host file"
+cp "$hostpath"hosts "$hostpath"hosts-backup-$(date +%y-%m-%d)--$(date +"%T")
+rm "$hostpath"hosts
+echo "Replace current host file with template"
+cp ./hosts-template "$hostpath"hosts
+
+
+################
+## Remplace variables with IP address
+
+echo "Set IP addresses to:" $ip
+sed -i -e "s/dynamicip/$ip/g" "$hostpath"hosts
+echo "Done :)"
